c7573288fd5b84829bc81c3b739b40053730d90d
Hritikbansal <hbansal10n@gmail.com>
2018-07-09 13:24:45 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,3 +1,4 @@
+#Simple code to convert any json format containing file to csv format
 import pandas as pd 
 #pd.read_json("filename in json format").to_csv("filename in csv format")
 pd.read_json("file_route_12.txt").to_csv("file_route_12.csv")
